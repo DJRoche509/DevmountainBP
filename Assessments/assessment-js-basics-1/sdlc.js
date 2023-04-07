f0c71160059424eef2e970ffa63b6e94a74dfffb
DJRoche509 <davjroche@gmail.com>
2023-04-07 18:13:08 -0500

    Enter definition for each SDLC elements.

--- Assessments/assessment-js-basics-1/sdlc.js
+++ Assessments/assessment-js-basics-1/sdlc.js
@@ -12,6 +12,16 @@
 // Testing & Integration
 // Planning
 
+//Correct order of the SDLC goes as follow:
+/*
+ * Planning
+ * Analysis of Requirements
+ * Design
+ * Implementation
+ * Testing & Integration
+ * Maintain
+*/
+
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -19,3 +29,28 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const analysis_of_Requierements = 'This the phase where all the relevant information is collected from the customer to develop a product as per their expectation. Any ambiguities must be resolved in this phase only' ;
+
+const design = 'In this third part of the phase, the requirement gathered in the SRS document is used as an input and software architecture that is used for implementing system development is derived' ;
+
+const implementation = 'Implementation/Coding starts once the developer gets the Design document. The Software design is translated into source code. All the components of the software are implemented in this phase' ;
+
+const testingAndIntegration = 
+    `Testing starts once the coding is complete and the modules are released for testing. In this phase, the developed software is tested thoroughly and any defects found are assigned to developers to get them fixed.
+     Retesting, regression testing is done until the point at which the software is as per the customer’s expectation. Testers refer SRS document to make sure that the software is as per the customer’s standard. 
+                           
+    Once the product is tested, it is deployed in the production environment where first UAT (User Acceptance testing) is done depending on the customer expectation.
+                               
+    In the case of UAT, a replica of the production environment is created and the customer along with the developers does the testing. If the customer finds the application as expected, then sign off is provided by the customer to go live.` ;
+
+const maintain = 'After the integration or deployment of a product on the production environment, maintenance of the product takes place. if any issue comes up and needs to be fixed or any enhancement is to be done is taken care by the developers' ;
+
+
+const sdlcArray = [planning, analysis_of_Requierements, design, implementation, testingAndIntegration, maintain] ;
+
+sdlcArray.forEach(element => {
+    console.log (element);
+    console.log(`\n-----------------------------------------------------------------------\n`)   
+});
+
