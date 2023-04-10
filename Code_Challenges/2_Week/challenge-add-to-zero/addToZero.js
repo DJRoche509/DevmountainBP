cdbc3abdc455380c6f81d2b005cd1b45bd43fefb
DJRoche509 <davjroche@gmail.com>
2023-04-10 02:00:57 -0500

    Implement workable solution in addToZero.js

--- Code_Challenges/2_Week/challenge-add-to-zero/addToZero.js
+++ Code_Challenges/2_Week/challenge-add-to-zero/addToZero.js
@@ -2,3 +2,29 @@
 let array = [28, 43, -12, 30, 4, 0, 12]
 
 // Write your solution below:
+
+function addToZero(arr){
+    // Create two variables m and n.
+    let m , n ; 
+    for (n=0; n<arr.length; n++){
+        m = 0 ;
+        while (m < n) {
+            if (arr[m] + arr[n]=== 0){
+                console.log(arr[m], arr[n]); 
+                console.log(true);
+                console.log(`${'-'.repeat(30)}\n`);
+                return ;
+            }   else{
+                m++ ;
+            } 
+        }    
+    }
+    console.log(!true) ;
+    console.log(`${'-'.repeat(30)}\n`);
+}
+
+
+addToZero(array);                       // Should log "true"
+addToZero([0, 1, 2, 3, 4, 5]) ;         // Should log "false"
+addToZero([1, 4, 11, 2, 37, -4]);       // Should log "true"
+addToZero([0, 21, 33, 6, 0, -9]);       // Should log "true"
