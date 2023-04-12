cf1916c55a9a740d22fd9708539e8cb40193da58
DJRoche509 <davjroche@gmail.com>
2023-04-12 12:44:35 -0500

    Implement callback to higher-order function

--- Code_Challenges/2_Week/functions-2-demo/starter.js
+++ Code_Challenges/2_Week/functions-2-demo/starter.js
@@ -3,7 +3,27 @@
 ////////////////////////
 
 // CODE HERE
+const add = (num1, num2) => num1 + num2 ;
 
+const substract = (num1, num2) => num1 - num2 ;
+
+const multiply = (num1, num2) => num1 * num2 ;
+
+const divide = (num1, num2) => num1 / num2 ;
+
+const calculator = (num1, num2, cb) => {
+  // checking to see if input is coercible to a number
+  if (+num1 && +num2){
+    num1 = +num1;
+    num2 = +num2;
+    return cb(num1, num2) ;
+  } else {
+    return console.log('Please, provide numbers only.');
+  }
+} 
+
+console.log(calculator(10,2,divide));       // Should Log out "5"
+console.log(calculator(10,'3',multiply));  // Should Log out "30"
 
 ///////////////////////
 ////// PET STORE //////
@@ -64,7 +84,58 @@ const catProducts = [
 ]
 
 // CODE HERE
+//APPLY DISCOUNT BY PERCENTAGE OR RATE,  we have to pass discount as decimals
+const applyPercentDiscount = (product, discount) => {
+  product.displayPrice = product.basePrice * (1 - discount)
+}
 
+const applyFlatDiscount = (product, discount) => {
+  product.displayPrice = product.basePrice - discount;
+}
+
+// higher-order method that allows to rite a loop once and apply discounts
+
+const applyDiscounts = (arr, cb, discount) => {
+  for (let i = 0; i < arr.length; i++){
+    cb(arr[i], discount)
+  }
+}
+
+// applyDiscounts(dogProducts, applyFlatDiscount, 1) ;
+// applyDiscounts(catProducts, applyPercentDiscount, .20)
+// console.log(dogProducts);
+// console.log(catProducts);
+
+
+
+//APPLY DISCOUNT  BY CATEGORIES
+const discountByCategories = (arr, cb, discount, category) => {
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i].category === category){
+      cb(arr[i], discount)
+    }
+  }
+}
+// discountByCategories(dogProducts, applyFlatDiscount, 2,1);
+// discountByCategories(catProducts,applyPercentDiscount,.10, 2)
+
+// console.log(dogProducts);
+// console.log(catProducts);
+
+
+
+//APPLY DISCOUNT ACORDING TO INVENTORY
+const discountByInventory = (arr, cb, discount, amount) => {
+  for (let i = 0; i < arr.length; i++ ){
+    if (arr[i].inventory > amount){
+      cb(arr[i], discount )
+    }
+  }
+}
+discountByInventory(dogProducts, applyFlatDiscount, 2,75 );
+discountByInventory(catProducts,applyPercentDiscount,.10, 75);
+console.log(dogProducts);
+console.log(catProducts);
 
 ////////////////////////
 ////// SANDWICHES //////
