17c9b9f64928748e4ed0bcc7b4d3c4015655611d
DJRoche509 <davjroche@gmail.com>
2023-04-12 12:39:50 -0500

    Implement solution for hasUniqueChars.js

--- Code_Challenges/2_Week/challenge-has-unique-char/hasUniqueChars.js
+++ Code_Challenges/2_Week/challenge-has-unique-char/hasUniqueChars.js
@@ -1,1 +1,43 @@
-// Write your code below
+// Has Unique Characters
+
+/*
+ * Given a word, return true if that word contains only unique characters. Return false otherwise.  
+ * For example:
+ *
+ * hasUniqueChars("Monday")
+ * // returns true
+ * 
+ * hasUniqueChars("Moonday")
+ * // returns false
+ * 
+ * 
+ * Uppercase and lowercase letters should be considered separately:
+ * 
+ * hasUniqueChars("Bob")
+ * // returns true
+ * 
+ */
+
+
+const checkUniqString = wordStr => {
+    const strSets= new Set(wordStr);
+    // console.log(strSets.keys());
+    if (wordStr.length === strSets.size ) {
+        return true;
+    }  
+    return false;
+}
+
+/*      Alternatively, we could do:
+
+const checkUniqString = (wordStr) => {
+    const strSets= new Set([]);
+    for (let i = 0; i < wordStr.length; i++){
+        strSets.add(wordStr[i]) ;
+    }
+    return strSets.size === wordStr.length;
+}
+ */
+
+console.log(checkUniqString("Monday"));
+console.log(checkUniqString("Moonday"));
