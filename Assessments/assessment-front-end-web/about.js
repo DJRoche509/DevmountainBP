97c64602347b3b24d2d4cf31fecee21434ba95d8
DJRoche509 <davjroche@gmail.com>
2023-04-23 19:25:25 -0500

    Added mouseover and eventListener w/ cb functions

--- Assessments/assessment-front-end-web/about.js
+++ Assessments/assessment-front-end-web/about.js
@@ -4,11 +4,20 @@ console.log("hello world");
 
 function handleSubmit(evt) {
 	evt.preventDefault();
-	
-	console.log('form submit');
+	alert('The form has been submitted successfully') ;
 }
 
+const mouseOverImage = evt =>{
+	evt.preventDefault();
+	// alert(`I'm hovering on the pic`);
+	let listems = document.getElementById('compliments').getElementsByTagName('li'); 
+	let i = Math.floor(Math.random()*listems.length) ;
+	alert(listems[i].innerText );
+	
+}
 
 let form = document.querySelector('#contact');
+let pictureOnForm = document.querySelector('img');
 
 form.addEventListener('submit', handleSubmit);
+pictureOnForm.addEventListener('mouseover', mouseOverImage);
