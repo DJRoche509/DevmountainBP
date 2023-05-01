0b1d6bcec023ff84f75ae5485055f2b45f39774d
DJRoche509 <davjroche@gmail.com>
2023-05-01 08:57:52 -0500

    update controller.js

--- Assessments/assessment-backend/server/controller.js
+++ Assessments/assessment-backend/server/controller.js
@@ -4,10 +4,35 @@ module.exports = {
         const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
       
         // choose random compliment
-        let randomIndex = Math.floor(Math.random() * compliments.length);
-        let randomCompliment = compliments[randomIndex];
+        let randomCompliment = compliments[getRandomIndex(compliments)];
       
         res.status(200).send(randomCompliment);
+    },
+
+    getFortune: (req, res) => {
+        const fortunes = ['A faithful friend is a strong defense.', 'An inch of time is an inch of gold.', 'Competence like yours is underrated.',
+                            'Courtesy begins in the home.', 'Keep your face to the sunshine and you will never see shadows.','Your mind is your greatest asset.']
+    
+        // Choose random fortune
+        let randomFortune = fortunes[getRandomIndex(fortunes)];
+        
+        res.status(200).send(randomFortune);
+    },
+
+    submitHandler: (req, res) => {
+        let {input} = req.body
+        !input ? res.status(400).send('Sorry, try again with a single valid WORD.')
+               : data.push(req.body) 
+               res.status(200).send(input); 
     }
 
+}
+
+/**
+ *  getRandomIndex() is used to automatically calculate and return a random number
+ * @param {string} arr - array pass to the function getRandomIndex 
+ * @returns {int} - A random number within the length of the arrray received by getRandomIndex()
+ */
+function getRandomIndex(arr){
+    return Math.floor(Math.random()*arr.length);
 }
