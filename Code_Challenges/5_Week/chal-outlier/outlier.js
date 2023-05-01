afbecb9e9e7accbbcef46a2fcf0c1a3b27534120
DJRoche509 <davjroche@gmail.com>
2023-05-01 10:45:47 -0500

    Implement solution for outlier.js

--- Code_Challenges/5_Week/chal-outlier/outlier.js
+++ Code_Challenges/5_Week/chal-outlier/outlier.js
@@ -16,3 +16,25 @@
 // if (arr[0]%2 ===0 and arr[1]%2 ===0)
 // for loop ---> i =0  arr[i]%2 ===1
 
+/**
+ * 
+ * @param {int} arr 
+ * @returns {int} 
+ */
+function outlier(arr){
+    let num ;
+    let num1 =arr[0],  num2=arr[1], num3=arr[2] ;
+    if (num1 % 2 === 0 && (num2 % 2 === 0 || num3%2 ===0)){
+        num = arr.filter(ans=>ans%2 ===1)
+        }
+    else{
+       num = arr.filter(ans =>ans%2 ===0)
+    }
+    return num[0]
+}
+
+console.log(outlier([160, 3, 1719, 19, 11, 13, -21]));
+// Should return: 160 (the only even number)
+
+console.log(outlier([2, 4, 0, 100, 4, 11, 2602, 36]));
+// Should return: 11 (the only odd number)
