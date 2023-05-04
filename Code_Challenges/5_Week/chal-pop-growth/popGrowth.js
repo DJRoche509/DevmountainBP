c454772fd6529e02a7081ff568f7c2823d1b322f
DJRoche509 <davjroche@gmail.com>
2023-05-04 01:12:26 -0500

    Implement working solutions for popGrowth.js

--- Code_Challenges/5_Week/chal-pop-growth/popGrowth.js
+++ Code_Challenges/5_Week/chal-pop-growth/popGrowth.js
@@ -5,11 +5,14 @@
  * 
  * Concepts: Math, Loops
  *
- * In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
+ * In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases 
+ * by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years 
+ * does the town need to see its population greater or equal to p = 1200 inhabitants?
  * 
  * At the end of the first year there will be: 1000 + 1000 * 0.02 + 50 => 1070 inhabitants
  * 
- * At the end of the 2nd year there will be: 1070 + 1070 * 0.02 + 50 => 1141 inhabitants (** number of inhabitants is an integer **)
+ * At the end of the 2nd year there will be: 1070 + 1070 * 0.02 + 50 => 1141 inhabitants (** number of inhabitants 
+ * is an integer **)
  * 
  * At the end of the 3rd year there will be: 1141 + 1141 * 0.02 + 50 => 1213
  * 
@@ -25,8 +28,29 @@
  * 
  * Examples:
  * 
- * nbYear(1500, 5, 100, 5000) --> 15
- * 
+ * nbYear(1500, 5, 100, 5000) --> 15  
  * nbYear(1500000, 2.5, 10000, 2000000) --> 10
- * Note: Don’t forget to convert the percent parameter as a percentage in the body of your function: if the parameter percent is 2 you have to convert it to 0.02.
+ * 
+ * Note: Don’t forget to convert the percent parameter as a percentage in the body of your function: if the parameter 
+ * percent is 2 you have to convert it to 0.02.
  */
+
+function nbYear (p0, prcnt, aug, p) {
+    let countYear= 0;
+    while ( p >= p0 ){ 
+        countYear++;
+        p0 = p0 + (Math.floor(p0 * prcnt / 100)) + aug ;
+    }
+    return countYear;
+}
+
+// Alternative solution using for loop
+// function nbYear(p0, percent, aug, p) {
+//     for (var y = 0; p0 < p; y++) {
+//         p0 = p0 * (1 + percent / 100) + aug;
+//     }
+//     return y;
+// }
+
+console.log(nbYear(1500, 5, 100, 5000));
+console.log(nbYear(1500000, 2.5, 10000, 2000000));
