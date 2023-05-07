a48a0107aece8d3b7d7d6a53dfe88d88ef231158
DJRoche509 <davjroche@gmail.com>
2023-05-07 13:28:16 -0500

    Updated makeUnique.js with instructions info

--- Code_Challenges/4_Week/chal-make-unique-string/makeUnique.js
+++ Code_Challenges/4_Week/chal-make-unique-string/makeUnique.js
@@ -1,1 +1,16 @@
-// Write your solution below:
+/*
+ * Make Unique String
+ * Difficulty: Medium
+ * 
+ * Concepts: Loops, Conditionals, String Methods
+ * 
+ * Write a function to remove all duplciate letters from a provided string. The string will only 
+ * contail lowercase letters between a - z. The string will not contain spaces.
+ * 
+ * For example: 
+ * >>> makeUnique('helloworld')
+ * // helowrd
+ * 
+ * >>> makeUnique('iwanttoclimbamountain')
+ * // iwantoclmbu
+ */
