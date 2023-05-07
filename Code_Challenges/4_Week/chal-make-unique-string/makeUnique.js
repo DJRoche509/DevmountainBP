861736e9c65d4e0fd88f4dd1eb9316e2bdb9eab0
DJRoche509 <davjroche@gmail.com>
2023-05-07 16:39:11 -0500

    Implement working solutions for makeUnique.js

--- Code_Challenges/4_Week/chal-make-unique-string/makeUnique.js
+++ Code_Challenges/4_Week/chal-make-unique-string/makeUnique.js
@@ -14,3 +14,28 @@
  * >>> makeUnique('iwanttoclimbamountain')
  * // iwantoclmbu
  */
+
+function makeUnique(wordString){
+    let result = '';
+    for (let i = 0; i < wordString.length; i++){
+        if (!result.includes(wordString[i])){
+            result += wordString[i];
+        }
+    }
+    return result;
+}
+
+console.log(makeUnique('helloworld'));
+// helowrd
+
+console.log(makeUnique('iwanttoclimbamountain'));
+// iwantoclmbu
+
+
+/**
+ *       Solution 2:
+ * function makeUnique(wordString) {
+ *      let strSet = new Set(wordString.split(''))
+ *      return [...strSet].join('')
+ * }
+ */
