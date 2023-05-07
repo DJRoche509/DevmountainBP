f7249ccd6c81a05896a5c80fc95a23f9f1452bca
DJRoche509 <davjroche@gmail.com>
2023-05-06 19:52:05 -0500

    Implement fetch res from dictAPI + add camelWord()

--- Assessments/assessment-backend/client/main.js
+++ Assessments/assessment-backend/client/main.js
@@ -7,7 +7,7 @@ const newDiv = document.getElementById('newDiv') ;
 const baseURL = "http://localhost:4000/api/myApp" ;
 
 // Query from a dictionnary API
-const url = "https://api/dictionaryapi.dev/api/v2/entries/en/" ;
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/" ;
 const res = document.getElementById('result');
 const sound = document.getElementById('sound');
 
@@ -35,14 +35,35 @@ const setUserInput = body => axios.post(`${baseURL}/sections`, body).then(myAppC
     
 function submitHandler (e) {
     e.preventDefault();
-    let input = document.querySelector('.userIn');
-    fetch(`${url}${input.value}`)
+    let input = document.querySelector('.userIn'); 
+    const fetchData = fetch(`${url}${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            res.innerHTML = `
+                <div class = 'word'>
+                    <h3>
+                        <span style="color:black;">${camelWord(data[0].word)}</span> <span style="font-size:10px"> definition</span></h3>
+                    <button> 
+                        <i class="fas fa-volume-up"></i>
+                    </button>
+                </div>
+                <div class="details"><p>${data[0].meanings[0].partOfSpeech}</p><p>/${data[0].phonetic}/</p></div>
+                <p class='word-meaning'> ${data[0].meanings[0].definitions[0].definition} </p>
+                <p class='word-example'> ${data[0].meanings[0].definitions[0].example || ""} </p> 
+            `;
+            //
+            })
+        .catch(() => {
+            res.innerHTML = `<h3 class="error">Try again. Could not find "${input.value}"</h3>`;
+        });
+        
 
     let bodyObj = {
         sentiment: input.value,
     }
 
-    setUserInput(bodyObj);
+    setUserInput(bodyObj, fetchData);
 
     input.value = '';
 }
@@ -51,12 +72,16 @@ function createSection(section) {
     const aSection = document.createElement('div');
     const aFeeling = document.createElement('h3') ;
     aSection.innerHTML = '<button type="button" class="btn-close" title="Click to delete" aria-label="Close">X</button>';
-    aFeeling.innerText = section.charAt(0).toUpperCase() + section.slice(1).toLowerCase()+' Section'; console.log(aFeeling);
+    aFeeling.innerText = camelWord(section)+' Section'; console.log(aFeeling);
     aSection.appendChild(aFeeling);
-    aSection.classList.add('buttons')
+    aFeeling.classList.add('sectionText')
+    newDiv.classList.add('resultBox')
     newDiv.appendChild(aSection)
 }
 
+function camelWord(word){
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+}
 
 function displaySections(arrSection) {
     console.log(arrSection);
