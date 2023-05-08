b7f675f0d1ac6f364dcba76e9b4c8e7d8af82545
DJRoche509 <davjroche@gmail.com>
2023-05-08 12:57:21 -0500

    Comment back in API get/post/delete calls

--- Code_Challenges/5_Week/unit-5-review-demo/server/index.js
+++ Code_Challenges/5_Week/unit-5-review-demo/server/index.js
@@ -17,11 +17,11 @@ const {
     deleteFighter
 } = require('./controller.js')
 
-app.post('/seed', seed)
-// app.post('/fighter', createFighter)
-// app.post('/weapon', createWeapon)
-// app.get('/fighters-list', getFightersList)
-// app.get('/fighters-and-weapons', getFightersWeapons)
-// app.delete('/weapon/:id', deleteWeapon)
+// app.post('/seed', seed)
+app.post('/fighter', createFighter)
+app.post('/weapon', createWeapon)
+app.get('/fighters-list', getFightersList)
+app.get('/fighters-and-weapons', getFightersWeapons)
+app.delete('/weapon/:id', deleteWeapon)
 
 app.listen(PORT, () => console.log(`up on ${PORT}`))
