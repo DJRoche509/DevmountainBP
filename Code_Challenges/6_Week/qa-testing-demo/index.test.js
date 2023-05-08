8a6d0f7e246a758ede8583a49e299ff7beb5c3a0
DJRoche509 <davjroche@gmail.com>
2023-05-08 15:07:03 -0500

    Implement test case functions

--- Code_Challenges/6_Week/qa-testing-demo/index.test.js
+++ Code_Challenges/6_Week/qa-testing-demo/index.test.js
@@ -14,4 +14,72 @@ let testData = {
     length: 65,
     floor: 5,
     roomNumber: 3
-  }
+  };
+
+  //basic test
+  test('should run basic test', () =>{
+    expect(2).toBe(2);
+  });
+  //to showcase we can use javasript functions and access data from testData
+  test('first name should be patten', () => {
+    expect(testData.firstName.toLowerCase()).toBe('patten');
+  });
+
+  //describe-block for formatTitle
+  describe('formatTitle test', () => {
+
+    test("formatTitle should return a string", () => {
+      let formattedTitle = formatTitle(testData.title)
+      expect(typeof formattedTitle).toBe('string')   
+    });
+
+    test("formatTitle should format the string correctly", () => {
+      let formattedTitle = formatTitle(testData.title)
+      expect(formattedTitle).toBe('Nulla Ac')   
+    });
+  });
+
+  //describe-block for shorten-bio
+  describe('shorten-bio tests', () => {
+
+    test('shorten-bio shortens a string', () => {
+      let shortBio = shortenBio(testData.bio)
+      expect(shortBio.length).toBeLessThan(testData.bio.length)
+    })
+
+    test('shorten-bio adds .. at the end', () => {
+      let shortBio = shortenBio(testData.bio)
+      // expect(shortBio).toContain('...')
+      expect(shortBio.endsWith('...')).toBe(true)
+    })
+  })
+
+//describe block for convert-length
+describe('convert length tests', () => {
+
+  test('convertLength returns an array with two items in it', () => {
+    let converted = convertLength(testData.length)
+    // expect(converted.length).toBe(2)
+    expect(converted).toHaveLength(2)
+    
+  })
+  
+  test('convertLength can handle numbers below 60', () => {
+    let converted = convertLength(40)
+    expect(converted[1]).toEqual(40)
+    expect(converted[0]).toEqual(0)
+  })
+})
+
+test ('toBE vs toEqual', () => { 
+  const myObj = {myNum:4}
+  const myObjTwo = {myNum:4}
+  expect(myObj).toEqual(myObjTwo)
+})
+
+// This test should because toBe() checks for equality of everything, including value and memory location
+test ('toBE vs toEqual', () => { 
+  const myObj = {myNum:4}
+  const myObjTwo = {myNum:4}
+  expect(myObj).toBe(myObjTwo)
+})
