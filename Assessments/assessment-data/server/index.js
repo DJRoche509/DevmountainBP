7118362928e81aba20af141113ff5298ba0e1ac1
DJRoche509 <davjroche@gmail.com>
2023-05-08 03:27:55 -0500

    Comment back in get, post, and delete app calls

--- Assessments/assessment-data/server/index.js
+++ Assessments/assessment-data/server/index.js
@@ -1,7 +1,8 @@
-require('dotenv').config()
 const express = require('express')
-const app = express()
 const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
 const {SERVER_PORT} = process.env
 const {seed, getCountries, getCities, createCity, deleteCity} = require('./controller.js')
 
@@ -9,14 +10,14 @@ app.use(express.json())
 app.use(cors())
 
 // DEV
-app.post('/seed', seed)
+// app.post('/seed', seed)
 
 // COUNTRIES
-// app.get('/countries', getCountries)
+app.get('/countries', getCountries)
 
 // CITIES
-// app.post('/cities', createCity)
-// app.get('/cities', getCities)
-// app.delete('/cities/:id', deleteCity)
+app.post('/cities', createCity)
+app.get('/cities', getCities)
+app.delete('/cities/:id', deleteCity)
 
 app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`))
