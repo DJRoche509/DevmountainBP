e768e1c76df2ecffb32581ae24525813f48769f8
DJRoche509 <davjroche@gmail.com>
2023-05-10 03:42:29 -0500

    Implement working solutions for romanNumerals.js

--- Code_Challenges/6_Week/chal-roman-numerals/romanNumerals.js
+++ Code_Challenges/6_Week/chal-roman-numerals/romanNumerals.js
@@ -35,3 +35,87 @@
  * 'LXXXXVIIII'
  */
 
+/**
+ * 
+ * @param {Integer} decimal 
+ * @returns {string}
+ */
+function toRoman(decimal) {
+    let romanStr = '';
+    let decimalRoman = {
+        1: 'I',
+        5: 'V',
+        10:'X',
+        50:'L',
+        100:'C',
+        500:'D',
+        1000:'M'
+    }
+
+    while (decimal > 0){
+        if (decimal ===1000){
+            romanStr = decimalRoman['1000'];
+            decimal -=1000;
+        } else if (decimal >= 500){
+            romanStr += decimalRoman['500'];
+            decimal-=500;
+        } else if (decimal >= 100){
+            romanStr += decimalRoman['100'];
+            decimal-=100;
+        } else if (decimal >= 50){
+            romanStr += decimalRoman['50'];
+            decimal-=50;
+        } else if (decimal >= 10){
+            romanStr += decimalRoman['10'];
+            decimal-=10;
+        } else if (decimal >= 5){
+            romanStr += decimalRoman['5'];
+            decimal-=5;
+        } else {
+            romanStr += decimalRoman['1'];
+            decimal-=1;
+        } 
+    }
+    return romanStr;
+}
+
+
+
+// This Solution methods converts to modern Roman numerals, where substraction is used. Ex: 4 = 'IV" --but NOT 'IIII' 
+// function toRoman(decimal) {
+//     let romanStr = '';
+//     while (decimal > 0){
+//         if (decimal === 1000){
+//             romanStr = decimalRoman['1000']; decimal-=1000
+//         }else if (decimal>=500){
+//             if (decimal > 899){
+//                 romanStr += decimalRoman['100']+decimalRoman['1000'] ; decimal -=900
+//             } else { romanStr += decimalRoman['500']; decimal-=500 }
+//         } else if(decimal >= 100){
+//             if (decimal > 399){
+//                 romanStr += decimalRoman['100']+decimalRoman['500']; decimal -= 400
+//             } else { romanStr += decimalRoman['100']; decimal -=100  }
+//         } else if (decimal >= 50){
+//             if (decimal > 89){
+//                 romanStr += decimalRoman['10']+decimalRoman['100']; decimal -= 90
+//             } else {  romanStr += decimalRoman['50']; decimal -=50 }
+//         } else if (decimal >= 10){
+//             if (decimal > 39){
+//                 romanStr += decimalRoman['10']+decimalRoman['50']; decimal -= 40
+//             } else { romanStr += decimalRoman['10']; decimal-=10 }
+//         }else if (decimal >= 5){
+//             if (decimal > 8){
+//                 romanStr += decimalRoman['1']+decimalRoman['10']; decimal -= 9
+//             }else{ romanStr += decimalRoman['5']; decimal -=5 }
+//         }else {
+//             if (decimal>3){ romanStr += decimalRoman['1']+decimalRoman['5']; decimal -=4 }
+//             else { romanStr += decimalRoman['1']; decimal-=1}
+//         }
+//    }return romanStr;
+// }
+
+console.log(toRoman(99));
+// 'LXXXXVIIII'
+
+console.log(toRoman(267));
+// 'CCLXVII'
