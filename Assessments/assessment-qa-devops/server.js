5d2f9de1f82a74d2902f0bec54376e29dd817318
DJRoche509 <davjroche@gmail.com>
2023-05-16 12:33:44 -0500

    Imported dotenv and rollbar + Added rollbar events

--- Assessments/assessment-qa-devops/server.js
+++ Assessments/assessment-qa-devops/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const bots = require("./src/botsData");
 const shuffle = require("./src/shuffle");
@@ -7,6 +8,18 @@ const playerRecord = {
   losses: 0,
 };
 const app = express();
+const { ROLLBAR_ACCESS_TOKEN } = process.env;
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: `${ROLLBAR_ACCESS_TOKEN}`,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
 // Set up middleware
 app.use(express.static(`${__dirname}/public`));
@@ -42,8 +55,10 @@ const calculateHealthAfterAttack = ({ playerDuo, compDuo }) => {
 
 app.get("/api/robots", (req, res) => {
   try {
+    rollbar.info('Someone requested all robots');
     res.status(200).send(bots);
   } catch (error) {
+    rollbar.warning('Failed to display robots. ERROR!')
     console.error("ERROR GETTING BOTS", error);
     res.sendStatus(400);
   }
@@ -52,6 +67,7 @@ app.get("/api/robots", (req, res) => {
 app.get("/api/robots/shuffled", (req, res) => {
   try {
     let shuffled = shuffle(bots);
+    rollbar.info('Someone shuffled the robots to play with');
     res.status(200).send(shuffled);
   } catch (error) {
     console.error("ERROR GETTING SHUFFLED BOTS", error);
@@ -71,14 +87,18 @@ app.post("/api/duel", (req, res) => {
     // comparing the total health to determine a winner
     if (compHealth > playerHealth) {
       playerRecord.losses += 1;
+      rollbar.log(`Player lost ${playerRecord.losses} time(s) already`);
       res.status(200).send("You lost!");
     } else if (compHealth === playerHealth){ 
+      rollbar.warning(`There has been a tie. Win is neutral`);
       res.status(200).send("It's a tie!");
     }else {
       playerRecord.wins += 1;
       res.status(200).send("You won!");
+      rollbar.log(`Player won ${playerRecord.wins} time(s) already`);
     }
   } catch (error) {
+    rollbar.critical('Could not duel. Failed!')
     console.log("ERROR DUELING", error);
     res.sendStatus(400);
   }
@@ -86,6 +106,7 @@ app.post("/api/duel", (req, res) => {
 
 app.get("/api/player", (req, res) => {
   try {
+    rollbar.log(`Player record has been requested.`)
     res.status(200).send(playerRecord);
   } catch (error) {
     console.log("ERROR GETTING PLAYER STATS", error);
