ce0ff3ea700fdaa8cfd450ae044e26301ce22062
DJRoche509 <davjroche@gmail.com>
2023-05-20 01:45:32 -0500

    Implement working solutions for sortProduct.js

--- Code_Challenges/6_Week/chal-sort-product/sortProduct.js
+++ Code_Challenges/6_Week/chal-sort-product/sortProduct.js
@@ -1,5 +1,5 @@
 /**
- * Sort by Product
+ * SORT BY PRODUCT
  * 
  * Difficulty: Medium
  * 
@@ -13,7 +13,7 @@
  * 
  * Example:
  * 
- * Input: 
+ * Input: 23, 2, 3, 4, 5
  * 
  * Product of value and index:
  * 
@@ -29,3 +29,43 @@
  * 
  * Output: 2, 3, 4, 23, 5
  */
+
+/**
+ * 
+ * @param {number[]} - arr. The integer array passed in as argument.
+ * @returns {number[]} - sortedArr.  The returned integer arrray sorted by the product of index and value of arr.
+ */
+const sortProduct = (arr) => {
+    //map the values to an array of objects containing value, index, and product
+    const mapArr = arr.map((value, index) => {
+        return {value, index, product: value * (index +1)}
+    })
+    //sort the mapped array by produc in acending order
+    mapArr.sort((a,b) => a.product - b.product)
+    //map the sorted array back to an array of values only 
+    const sortedArr = mapArr.map(obj => obj.value)
+    return sortedArr
+}
+
+/**
+ *   SOLUTION 2
+ * 
+ * @param {number[]} - arrNum 
+ * @returns {number[]} 
+ *
+ * function sortProduct(arrNum) {
+ *     let newArrNum = [];
+ * 
+ *     for(let i=0; i<arrNum.length; i++){
+ *         let product = arrNum[i] * (i + 1)
+ *         let value = arrNum[i]
+ *         newArrNum.push({product, i, value}); 
+ *     }
+ *     newArrNum.sort((a, b) => a.product - b.product);
+ *     return newArrNum.map(el => el.value)
+ * }
+ * 
+ */
+
+
+console.log(sortProduct([23, 2, 3, 4, 5]));
