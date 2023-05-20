b2e01093d1f930834731e636a15442115eb822fd
DJRoche509 <davjroche@gmail.com>
2023-05-19 23:54:12 -0500

    Added challenge problem instruction

--- Code_Challenges/7_Week/chal-road-trip/tripTime.js
+++ Code_Challenges/7_Week/chal-road-trip/tripTime.js
@@ -1,3 +1,47 @@
+/**
+ * ROAD TRIP TIME
+ * 
+ * Difficulty:  Harder 
+ * Concepts:    Math, Higher Order Functions
+ * 
+ * You are planning a road trip (in a time before Google Maps) and want to know how long each segment of the trip is going to be. Given an array of objects like this:
+ * 
+ * {
+ *     name: `Jake's Great Shakes`,
+ *     speedLimits: [
+ *         {
+ *             distance: 100,
+ *             speedLimit: 75
+ *         },
+ *         {
+ *             distance: 84,
+ *             speedLimit: 70
+ *         },
+ *         {
+ *             distance: 20,
+ *             speedLimit: 75
+ *         }
+ *     ],
+ *     traffic: 30
+ * }
+ * 
+ * Update the given function so that the segmentTimes array in the result object contains the times for each segment of the trip in hours. You can round the times to whole numbers. 
+ * Also update the totalTime property to be the total time of the trip.
+ * 
+ * Additionally, because you like data, you want to know what the average speed limit for each segment of the trip of is.
+ * 
+ * Since you are traveling different distances at each speed, you’ll need to weight the averages. For example, given a trip segment where you travel 10 miles at 50 MPH and for 20 miles 
+ * at 35 MPH, the average weighted speed limit would be 39.9. If you just added 50 and 35 and divided by 2 for a typical average, you would get 42.5. The weighted average is lower since 
+ * you traveled a longer distance at the lower speed.
+ * 
+ * As you find these weighted averages, add them into the avgSpeedLimits array in the result object.
+ * 
+ * Return the result object from the function.
+ * 
+ * Hint: You can use the reduce method.
+ *
+ */
+
 const tripTime = (arr) => {
     let result = {
         avgSpeedLimits: [], 
