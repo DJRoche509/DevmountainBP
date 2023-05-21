ee938f3dabe9aba7fb42acd3e223576ac20de6b7
DJRoche509 <davjroche@gmail.com>
2023-05-20 23:59:05 -0500

    Comment back in API calls invokes

--- Code_Challenges/5_Week/sql-in-practice/server/index.js
+++ Code_Challenges/5_Week/sql-in-practice/server/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const {SERVER_PORT} = process.env
+
 const {
     getAllClients,
     getPendingAppointments,
@@ -17,13 +18,13 @@ app.use(express.json())
 app.use(cors())
 
 // USERS
-// app.get('/clients', getAllClients)
+app.get('/clients', getAllClients)
 
 // APPOINTMENTS
-// app.get('/pending', getPendingAppointments)
-// app.get('/upcoming', getUpcomingAppointments)
-// app.get('/appt', getPastAppointments)
-// app.put('/approve', approveAppointment)
-// app.put('/complete', completeAppointment)
+app.get('/pending', getPendingAppointments)
+app.get('/upcoming', getUpcomingAppointments)
+app.get('/appt', getPastAppointments)
+app.put('/approve', approveAppointment)
+app.put('/complete', completeAppointment)
 
-app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`))
+app.listen(SERVER_PORT, () => console.log(`Jamming on port: ${SERVER_PORT}`))
